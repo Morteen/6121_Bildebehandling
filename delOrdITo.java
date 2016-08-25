f2d0ee05711e73d8f720f351aee16058e29c89a8
Morteen <mortenolsen4@gmail.com>
2016-08-25 19:31:41 +0200

    Add files via upload

---
+++ delOrdITo.java
@@ -0,0 +1,26 @@
+/*Oppgave 1-c fra 2011
+*Dele et ord i to ved annen hver bokastav
+*/
+import static java.lang.System.*;
+import static javax.swing.JOptionPane.*;
+
+public class delOrdITo {
+
+  public static void main(String[] args) {
+   String innOrd,utOrd1,utOrd2;
+    innOrd= "srerirlrerbrårt";
+   utOrd1="";
+   utOrd2="";
+   for(int i=0;i<innOrd.length();i++){
+   	 innOrd.charAt(i);
+   	 if(i==0||i%2==0){
+   	 	utOrd1+=innOrd.charAt(i);
+   	 }else{
+   	 	utOrd2+=innOrd.charAt(i);
+   	 }
+   }
+   out.println(utOrd1);
+   out.println(utOrd2);
+  }//main
+
+}//class
